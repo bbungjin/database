d13b785c15467df645790b2694a535080b7b15bc
euicheon <sphaury@gmail.com>
2023-05-21 21:42:44 +0900

    마이페이지 끝

--- webapp/func/myPage.post.php
+++ webapp/func/myPage.post.php
@@ -3,12 +3,15 @@ session_start();
 if (isset($_SESSION['UserID']) === false){
     header("Location: ./login.php");
     exit();
-}   
+}       
 
 
-require_once("../../../inc/db.php");
+require_once ("../../../inc/db.php");
 
 $UserID = $_SESSION['UserID'];
+$UserInfo = db_select("select * from usertbl where UserID = ?", array($UserID));
+$UserInfo_count = db_select("select count(UserID) cnt from usertbl where UserID = ?", array($UserID));
+
 
 $Name = isset($_POST['Name']) ? $_POST['Name'] : null;
 $age = isset($_POST['age']) ? $_POST['age'] : null;
@@ -22,26 +25,35 @@ $major = isset($_POST['major']) ? $_POST['major'] : null;
 $phonenumber = isset($_POST['phonenumber']) ? $_POST['phonenumber'] : null;
 $weight = isset($_POST['weight']) ? $_POST['weight'] : null;
 
-$major = "컴퓨터공학과";
+
 
 
 // 이미 학번이 입력되어 있는지 체크 (학번이 기본키라 중복 입력되면 안됨)
 $schoolnum_count = db_select("select count(Name) cnt from usertbl where schoolnum = ?" , array($schoolnum));
-if ($schoolnum_count && $schoolnum_count[0]['cnt'] == 1){    
-    header("Location: ../myPage.php"); //이미 데베상에 존재하는 학번이면 myPage로 다시 이동 or 학번 중복 입력 알람
-    exit();
-}
 
 
 
-// 데이터 저장 
-db_insert("insert into usertbl values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", array($Name, $age, $Height, $SelfInfo, $sex, $region, $mbti, $schoolnum, $major, $phonenumber , $UserID, $weight));
+if ($UserInfo_count && $UserInfo_count[0]['cnt'] == 1) {
+    // 기존 정보가 있으면 업데이트
+    db_insert("update usertbl set Name=?, Age=?, Sex=?, Height=?, weight=?, schoolnum=?, phonenumber=?, mbti=?, region=?, SelfInfo=?, major=? where UserID=?",
+    array($Name, $age, $sex, $Height, $weight, $schoolnum, $phonenumber, $mbti, $region, $SelfInfo, $major, $UserID ));
+} else {
+    // 기존 정보가 없으면 삽입
+    if ($schoolnum_count && $schoolnum_count[0]['cnt'] == 1){   //usertbl에 UserID 데이터는 없는데 이미 유저가 입력한 학번이 존재한다 == 학번 중복이다. 
+        header("Location: ../myPage.php"); //이미 데베상에 존재하는 학번이면 myPage로 다시 이동 or 학번 중복 입력 알람
+        exit();
+    }
+    db_insert("insert into usertbl values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)", 
+    array($Name, $age, $Height, $SelfInfo, $sex, $region, $mbti, $schoolnum, $major, $phonenumber, $UserID, $weight));
+} 
 
 
 
-// 로그인 페이지로 이동
-header("Location: ../myPage.php");
+//모든 동작이 끝나면 마이페이지로 다시 이동
+header("Location: ../myPage.php")
 ?>
+
+
 
 <!-- myPage를 입력 페이지와 이미 입력한 정보 확인할 수 있는 페이지 두개로 나눌지 아니면 다른 방법을 찾을지
 문제 1. 어떻게 데베상의 정보를 끌어와서 마이페이지에 보여줄 것인가?
@@ -53,4 +65,7 @@ header("Location: ../myPage.php");
 문제 4 -> update usertbl set  Name = ? where UserID = ? 
 회원이 특정 정보를 수정하려고 한다 -> 수정 버튼을 누른다 -> 이미 입력된 정보는 그대로 보이고 변경 가능하도록 입력 칸이 활성화 된다.
 ->
+1. 최초 입력시 보여줄 마이페이지
+2. 입력 완료 후 후에 다시 마이페이지 들어갔을 때 보여줄 페이지
+3. 수정 버튼 눌렀을 때 보여줄 페이지
 -->
--- webapp/myPage.php
+++ webapp/myPage.php
@@ -1,14 +1,19 @@
 <?php
 session_start();
-if (isset($_SESSION['UserID']) === false){   //여기서 조건을 걸어서 페이지 분기 시키기? 
+require_once("../../inc/db.php");
+
+// 로그인 여부 확인
+if (isset($_SESSION['UserID']) === false){
     header("Location: ./login.php");
     exit();
 }   
-    require_once("../../inc/db.php");
 
-    $UserID = $_SESSION['UserID'];
+$UserID = $_SESSION['UserID'];
 
-    
+
+$UserInfo = db_select("select * from usertbl where UserID = ?", array($UserID));   //세션 UserID로 usertbl에서 유저 정보 불러오기
+$UserInfo_count = db_select("select count(UserID) cnt from usertbl where UserID = ?", array($UserID)); //usertbl의 UserID 개수 불러오기
+
 ?>
 
 <!DOCTYPE html>
@@ -31,28 +36,28 @@ if (isset($_SESSION['UserID']) === false){   //여기서 조건을 걸어서 페
     <hr class="border opacity-100">    
     <div class = "userInfo m-5">
         <form class="row g-3 needs-validation" id = "inputMyInfo" method = "POST" action = "./func/myPage.post.php">
-            <div class="col-md-6"> 
+            <div class="col-md-6">
                 <label for="inputName" class="form-label">이름</label>
-                <input type="text" class="form-control" id="inputName"  name = "Name" required> 
+                <input type="text" class="form-control" id="inputName" name = "Name" value = "<?php echo (($UserInfo_count[0]['cnt'] == 1) ? ($UserInfo[0]['Name']) : null);  ?>" required disabled> 
             </div>
             <div class="col-md-6">
                 <label for="inputAge" class="form-label">나이</label>
-                <input type="text" class="form-control" id="inputAge" name = "age" placeholder="숫자만 입력하세요." required >
+                <input type="text" class="form-control" id="inputAge" name = "age" placeholder="숫자만 입력하세요." value = "<?php echo (($UserInfo_count[0]['cnt'] == 1) ? ($UserInfo[0]['age']) : null);  ?>"  required >
                 <div class="invalid-feedback" id = "ageError">
                  나이를 입력해 주세요.
                 </div>
             </div>
             <fieldset class="row mb-1 pt-2">
                 <legend class="col-form-label col-sm-1">성별</legend>
-                <div class="col-sm-10 pt-2">
+                <div class="col-sm-5 pt-2">
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="sex" id="gridRadios1" value="M" checked>
+                        <input class="form-check-input" type="radio" name="sex" id="gridRadios1" value ="M" <?php echo (($UserInfo_count[0]['cnt'] == 1 && $UserInfo[0]['sex'] == 'M') ? "checked" : null);  ?>>
                         <label class="form-check-label" for="gridRadios1">
                         남자
                         </label>
                     </div>
                     <div class="form-check">
-                        <input class="form-check-input" type="radio" name="sex" id="gridRadios2" value="F">
+                        <input class="form-check-input" type="radio" name="sex" id="gridRadios2" value="F" <?php echo (($UserInfo_count[0]['cnt'] == 1 && $UserInfo[0]['sex'] == 'F') ? "checked" : null);  ?> >
                         <label class="form-check-label" for="gridRadios2">
                         여자
                         </label>
@@ -61,24 +66,25 @@ if (isset($_SESSION['UserID']) === false){   //여기서 조건을 걸어서 페
             </fieldset>
             <div class="col-md-6">
                 <label for="inputHeight" class="form-label">키</label>
-                <input type="text" class="form-control" id="inputHeight" name = "Height" placeholder="숫자만 입력하세요." required>
+                <input type="text" class="form-control" id="inputHeight" name = "Height" placeholder="숫자만 입력하세요." value = "<?php echo (($UserInfo_count[0]['cnt'] == 1) ? ($UserInfo[0]['Height']) : null);  ?>" required>
             </div>
             <div class="col-md-6">
                 <label for="inputWeight" class="form-label">몸무게</label>
-                <input type="text" class="form-control" id="inputWeight" name = "weight" placeholder="숫자만 입력하세요." required>
+                <input type="text" class="form-control" id="inputWeight" name = "weight" placeholder="숫자만 입력하세요." value = "<?php echo (($UserInfo_count[0]['cnt'] == 1) ? ($UserInfo[0]['weight']) : null);  ?>" required>
             </div>
             <div class="col-md-6">
                 <label for="inputSchoolNum" class="form-label">학번</label>
-                <input type="text" class="form-control" id="inputSchoolNum" name = "schoolnum" placeholder="학번을 입력하세요." required>
+                <input type="text" class="form-control" id="inputSchoolNum" name = "schoolnum" placeholder="학번을 입력하세요." value = "<?php echo (($UserInfo_count[0]['cnt'] == 1) ? ($UserInfo[0]['schoolnum']) : null);  ?>" required>
             </div>
             <div class="col-md-6">
                 <label for="inputPhoneNum" class="form-label">전화번호</label>
-                <input type="text" class="form-control" id="inputPhoneNum" name = "phonenumber" placeholder="공백이나 기호를 사용하지 않고 숫자만 입력하세요." required>
+                <input type="text" class="form-control" id="inputPhoneNum" name = "phonenumber" placeholder="공백이나 기호를 사용하지 않고 숫자만 입력하세요." value = "<?php echo (($UserInfo_count[0]['cnt'] == 1) ? ($UserInfo[0]['phonenumber']) : null);  ?>" required>
             </div>
             <div class="col-md-6">
                 <label for="inputMBTI" class="form-label">MBTI</label>
-                <select id="inputMBTI" name = "mbti" class="form-select">
-                <option selected>ISTJ</option>
+                <select id="inputMBTI" name = "mbti" class="form-select" >
+                <option selected><?php echo (($UserInfo_count[0]['cnt'] == 1) ? ($UserInfo[0]['mbti']) : null);  ?></option>
+                <option>ISTJ</option>
                 <option>ISFJ</option>
                 <option>INFJ</option>
                 <option>INTJ</option>
@@ -99,6 +105,7 @@ if (isset($_SESSION['UserID']) === false){   //여기서 조건을 걸어서 페
             <div class="col-md-6">
                 <label for="inputResidence"  class="form-label">거주지</label>
                 <select id="inputResidence" name = "region" class="form-select">
+                <option selected><?php echo (($UserInfo_count[0]['cnt'] == 1) ? ($UserInfo[0]['region']) : null);  ?></option>    
                 <option>서울특별시</option>
                 <option>부산광역시</option>
                 <option>대구광역시</option>
@@ -117,9 +124,42 @@ if (isset($_SESSION['UserID']) === false){   //여기서 조건을 걸어서 페
                 <option>제주특별자치도</option>
                 </select>
             </div>
+            <div class="col-md-6">
+                <label for="inputMajor" class="form-label">전공</label>
+                <select id="inputMajor" name = "major" class="form-select">
+                <option selected><?php echo (($UserInfo_count[0]['cnt'] == 1) ? ($UserInfo[0]['major']) : null)  ?></option> 
+                <option>산업디자인학과</option>
+                <option>실내디자인학과</option>
+                <option>패션디자인학과</option>
+                <option>시각영상디자인학과</option>
+                <option>미디어콘텐츠학과</option>
+                <option>조형예술학과</option>
+                <option>경영학과</option>
+                <option>경제통상학과</option>
+                <option>경찰학과</option>
+                <option>소방방재융합학과</option>
+                <option>문헌정보학과</option>
+                <option>유아교육과</option>
+                <option>사회복지학과</option>
+                <option>신문방송학과</option>
+                <option>동화한국어문화학과</option>
+                <option>영어문화학과</option>
+                <option>메카트로닉스공학과</option>
+                <option>컴퓨터공학과</option>
+                <option>녹색기술융합학과</option>
+                <option>응용화학과</option>
+                <option>간호학과</option>
+                <option>바이오의약학과</option>
+                <option>생명공학과</option>
+                <option>식품학과</option>
+                <option>뷰티화장품학과</option>
+                <option>스포츠건강학과</option>
+                <option>골프산업학과</option>
+                </select>
+            </div>
             <div class="col-md-12">
                 <label for="inputIntro" class="form-label">자기소개</label>
-                <textarea type="text" class="form-control" id="inputIntro" name = "SelfInfo" rows="5"></textarea>
+                <textarea type="text" class="form-control" id="inputIntro" name = "SelfInfo" rows="5"><?php echo (($UserInfo_count[0]['cnt'] == 1) ? ($UserInfo[0]['SelfInfo']) : null)  ?></textarea>
             </div>
             <!-- <div class="col-12">
                 <div class="form-check">
